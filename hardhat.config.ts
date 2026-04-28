1431983ae517a5146a3ebaf49fc3d42b0902bf93
Lê Học Minh <hocminh1998@gmail.com>
2026-04-29 00:14:10 +0300

    commit source code contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,39 +1,29 @@
-import "@nomicfoundation/hardhat-toolbox-mocha-ethers";
-import { configVariable, defineConfig } from "hardhat/config";
-import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
 
-export default defineConfig({
-  plugins: [hardhatToolboxMochaEthers],
+dotenv.config();
+
+const config: HardhatUserConfig = {
   solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
+    version: "0.8.28",
+    settings: {
+      evmVersion: "cancun",
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
     },
   },
   networks: {
-    hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
-    },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
-    },
     sepolia: {
-      type: "http",
-      chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
-});
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
+};
+
+export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,36 +1,54 @@
-import { network } from "hardhat";
+import { ethers } from "hardhat";
 import fs from "fs";
 import path from "path";
 
 async function main() {
-    const { ethers } = await network.connect();
+    // const { ethers } = await network.connect();
     const [deployer] = await ethers.getSigners();
 
     console.log("Deploying contracts with the account:", deployer.address);
 
+    // 1. Deploy SGDNFT
     const sgdNFT = await ethers.deployContract("SGDNFT", [deployer.address]);
     await sgdNFT.waitForDeployment();
     const sgdNFTAddress = await sgdNFT.getAddress();
-
     console.log("SGDNFT deployed to:", sgdNFTAddress);
 
+    // 2. Deploy RGDNFT
+    const rgdNFT = await ethers.deployContract("RGDNFT", [deployer.address]);
+    await rgdNFT.waitForDeployment();
+    const rgdNFTAddress = await rgdNFT.getAddress();
+    console.log("RGDNFT deployed to:", rgdNFTAddress);
+
+    // 3. Deploy GDMRegistry 
     const registry = await ethers.deployContract("GDMRegistry", [
         sgdNFTAddress,
         deployer.address,
     ]);
     await registry.waitForDeployment();
     const registryAddress = await registry.getAddress();
-
     console.log("GDMRegistry deployed to:", registryAddress);
 
-    const tx = await sgdNFT.setMinter(registryAddress);
-    await tx.wait();
+    // Configure Registry setting for RGDNFT (implement listRGDNFT)
+    await (await rgdNFT.setGDMRegistry(registryAddress)).wait();
+    console.log("RGDNFT linked to Registry:");
 
-    console.log("Minter set to registry:", registryAddress);
+    // Grant Minter permissions to the Reistry at SGDNFT
+    await (await sgdNFT.setMinter(registryAddress)).wait();
+    console.log("Minter set to registry: ", registryAddress);
+
+    // Granting Deployers permision to act as "authorized SC" for demo
+    await (await rgdNFT.authorizeSC(deployer.address, true)).wait();
+    console.log("Deployer authorized as SC for RGDNFT: ");
+    
+    // const tx = await sgdNFT.setMinter(registryAddress);
+    // await tx.wait();
+    // console.log("Minter set to registry:", registryAddress);
 
     const out = {
         network: (await ethers.provider.getNetwork()).chainId.toString(),
         SGDNFT_ADDRESS: sgdNFTAddress,
+        RGDNFT_ADDRESS: rgdNFTAddress,
         GDMREGISTRY_ADDRESS: registryAddress,
     };
 
