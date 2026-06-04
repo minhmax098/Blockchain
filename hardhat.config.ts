a60beb71a7ee8e5c3cc8a80a4f2ee8a4f321c76e
minhmax098 <hocminh1998@gmail.com>
2026-06-04 22:43:13 +0300

    commit source code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,6 +19,7 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      loggingEnabled: true,
     },
   },
   etherscan: {
